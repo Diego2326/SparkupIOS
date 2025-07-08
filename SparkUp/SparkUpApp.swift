2d3eb6e4fb9da4ccf6f498d0fbf7905720d8a4ee
diego <diegoe3b@gmail.com>
2025-07-08 07:45:17 -0600

    Initial Commit

---
+++ SparkUp/SparkUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkUpApp.swift
+//  SparkUp
+//
+//  Created by Diego on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SparkUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
